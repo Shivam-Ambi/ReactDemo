da88039db4db79dea111c1e3936f40cfd5f2d012
ShivamAmbi <shivam.ambicr7@gmail.com>
2022-02-09 03:17:25 +0530

    increment code implemented

--- demo-app/src/BtnComp.js
+++ demo-app/src/BtnComp.js
@@ -1,20 +1,37 @@
 import React, { useState } from 'react';
 function BtnComp(props) {
-    const [ defaultValue, setDefaultValue ] = useState();
+    let [defaultValue, setDefaultValue] = useState();
     function sendValue(e) {
-        console.log("Clicked", defaultValue);
-        props.recieveDefaultValue(defaultValue);
+        if (defaultValue) {
+            console.log("Clicked sendval ", defaultValue);
+            props.recieveDefaultValue1(defaultValue);
+        }
+        else {
+            setDefaultValue(props.countValue1);
+        }
+
     }
     function recieveValue(e) {
         console.log(e.target.value);
+        if (e.target.value === NaN) {
+            setDefaultValue(0);
+        }
         // defaultValue = e.target.value;
-        setDefaultValue( parseInt( e.target.value));
+        setDefaultValue(parseInt(e.target.value));
+    }
+    function incrementVal(e) {
+        props.incVal1();       //will increment default values only
+        if (defaultValue === NaN || defaultValue === undefined) {
+            defaultValue = props.countValue1;
+
+        }
+        else { setDefaultValue(defaultValue + 1); }
     }
     return (
         <div className='btnBox'>
-            <input type="text" onChange={recieveValue} value={ defaultValue }></input>
+            <input type="text" onChange={recieveValue} value={defaultValue}></input>
             <button onClick={sendValue}>submit</button>
-            <button >clickMe</button>
+            <button onClick={incrementVal}>clickMe</button>
         </div>
     )
 }
